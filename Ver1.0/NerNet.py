8af138fe57ad13e380b3f528e483d9ceffcfba6a
Tismoney <tismoney@mail.ru>
2017-04-23 13:00:38 +0300

    Add stable version of nernet

---
+++ Ver1.0/NerNet.py
@@ -0,0 +1,267 @@
+#!/home/paul/anaconda2/bin/python2.7
+
+
+import lasagne
+import theano
+from theano import tensor as T
+from lasagne.nonlinearities import *
+import numpy as np
+import time
+import os
+import pandas as pd
+
+from mnist import load_dataset
+
+from PyQt4.QtCore import QObject, pyqtSignal
+
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+	assert len(inputs) == len(targets)
+	if shuffle:
+	    indices = np.arange(len(inputs))
+	    np.random.shuffle(indices)
+	for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+	    if shuffle:
+	        excerpt = indices[start_idx:start_idx + batchsize]
+	    else:
+	        excerpt = slice(start_idx, start_idx + batchsize)
+	    yield inputs[excerpt], targets[excerpt]
+
+def architecture_one(input_X, weight = False, weig = None):
+	if (weight == False):
+		input_layer   = lasagne.layers.InputLayer   (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		drop_layer    = lasagne.layers.DropoutLayer (input_layer, p=0.2)
+		dense_1_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=200, nonlinearity=rectify, name = "Dense_1")
+		drop_layer    = lasagne.layers.DropoutLayer (dense_1_layer, p=0.2)
+		dense_2_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=100, nonlinearity=sigmoid, name = "Dense_2")
+		drop_layer    = lasagne.layers.DropoutLayer (dense_2_layer, p=0.2)
+		dense_3_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=50, nonlinearity=rectify, name = "Dense_3")
+		drop_layer    = lasagne.layers.DropoutLayer (dense_3_layer, p=0.2)
+		output_layer  = lasagne.layers.DenseLayer   (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output")
+	else:
+		input_layer   = lasagne.layers.InputLayer   (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		drop_layer    = lasagne.layers.DropoutLayer (input_layer, p=0.2)
+		dense_1_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=200, nonlinearity=rectify, name = "Dense_1", 
+		                                                W = weig[0], b = weig[1].reshape(weig[1].shape[0], ))
+		drop_layer    = lasagne.layers.DropoutLayer (dense_1_layer, p=0.2)
+		dense_2_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=100, nonlinearity=sigmoid, name = "Dense_2",
+		                                                W = weig[2], b = weig[3].reshape(weig[3].shape[0], ))
+		drop_layer    = lasagne.layers.DropoutLayer (dense_2_layer, p=0.2)
+		dense_3_layer = lasagne.layers.DenseLayer   (drop_layer, num_units=50, nonlinearity=rectify, name = "Dense_3", 
+		                                                W = weig[4], b = weig[5].reshape(weig[5].shape[0], ))
+		drop_layer    = lasagne.layers.DropoutLayer (dense_3_layer, p=0.2)
+		output_layer  = lasagne.layers.DenseLayer   (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output", 
+		                                                W = weig[6], b = weig[7].reshape(weig[7].shape[0], ))
+	return output_layer
+
+def architecture_two(input_X, weight = False, weig = None):
+	if (weight == False):
+		input_layer   = lasagne.layers.InputLayer   (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		drop_layer    = lasagne.layers.DropoutLayer (input_layer, p=0.2)
+		dense_layer   = lasagne.layers.DenseLayer   (drop_layer, num_units=800, nonlinearity=rectify, name = "Dense")
+		drop_layer    = lasagne.layers.DropoutLayer (dense_layer, p=0.5)
+		output_layer  = lasagne.layers.DenseLayer   (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output")
+	else:
+		input_layer   = lasagne.layers.InputLayer   (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		drop_layer    = lasagne.layers.DropoutLayer (input_layer, p=0.5)
+		dense_layer   = lasagne.layers.DenseLayer   (drop_layer, num_units=800, nonlinearity=rectify, name = "Dense",
+					                                    W = weig[0], b = weig[1].reshape(weig[1].shape[0], ))
+		drop_layer    = lasagne.layers.DropoutLayer (dense_layer, p=0.5)
+		output_layer  = lasagne.layers.DenseLayer   (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output",
+					                                    W = weig[2], b = weig[3].reshape(weig[3].shape[0], ))
+	return output_layer
+
+def architecture_three(input_X, weight = False, weig = None):
+	if (weight == False):
+		input_layer   = lasagne.layers.InputLayer    (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		conv_layer    = lasagne.layers.Conv2DLayer   (input_layer, num_filters = 32, filter_size = (5, 5), nonlinearity=rectify, name = "Conv2D")
+		maxpool_layer = lasagne.layers.MaxPool2DLayer(conv_layer, pool_size = (2,2), name = "MaxPool")
+		drop_layer    = lasagne.layers.DropoutLayer  (maxpool_layer, p=0.5) 
+		dense_layer   = lasagne.layers.DenseLayer    (drop_layer, num_units=256, nonlinearity=rectify, name = "Dense")
+		drop_layer    = lasagne.layers.DropoutLayer  (dense_layer, p=0.5)
+		output_layer  = lasagne.layers.DenseLayer    (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output")
+	else:
+		input_layer   = lasagne.layers.InputLayer    (shape=(None,1,28,28), input_var=input_X, name = "Input")
+		conv_layer    = lasagne.layers.Conv2DLayer   (input_layer, num_filters = 32, filter_size = (5, 5), nonlinearity=rectify, name = "Conv2D",
+					                                    W = weig[0], b = weig[1].reshape(weig[1].shape[0], ))
+		maxpool_layer = lasagne.layers.MaxPool2DLayer(conv_layer, pool_size = (2,2), name = "MaxPool")
+		drop_layer    = lasagne.layers.DropoutLayer  (maxpool_layer, p=0.5) 
+		dense_layer   = lasagne.layers.DenseLayer    (drop_layer, num_units=256, nonlinearity=rectify, name = "Dense",
+					                                    W = weig[2], b = weig[3].reshape(weig[3].shape[0], ))
+		drop_layer    = lasagne.layers.DropoutLayer  (dense_layer, p=0.5)
+		output_layer  = lasagne.layers.DenseLayer    (drop_layer,num_units = 10, nonlinearity=softmax, name = "Output",
+					                                    W = weig[4], b = weig[5].reshape(weig[5].shape[0], ))
+	return output_layer	
+
+
+class NerNet(QObject):
+	new_epoch = pyqtSignal(int, name='new_epoch')
+	def __init__(self, parent = None):
+		super(NerNet, self).__init__(parent)
+		self.train_X = 0
+		self.train_y = 0
+		self.test_X = 0
+		self.test_y = 0
+		self.val_X = 0
+		self.val_y = 0
+		self.batch_size = 50
+		self.num_epochs = 1 # it must be 10
+		self.acc = 0
+
+	def signalConnect(self, obj):
+		self.new_epoch.connect(obj)
+
+	def accuarcy_fun():
+		return
+
+	def pred_fun():
+		return
+
+	def init_data(self):
+		X_train,y_train,X_val,y_val,X_test,y_test = load_dataset()
+		self.train_X = X_train
+		self.train_y = y_train
+		self.test_X = X_test
+		self.test_y = y_test
+		self.val_X = X_val
+		self.val_y = y_val
+
+	def put_weights(self, weights, path = "Weight", conv = False):
+		for i, weight in enumerate(weights):
+			p = weight.get_value()
+			if ( (conv == True) and (i == 0) ):
+				p = p.reshape(32, 25)
+			matrix = pd.DataFrame(p)
+			matrix.to_csv(path + "/" + str(i) + ".csv")
+
+	def get_weight(self, path = "Weight", conv = False):
+		weights = []
+		num_files = len(os.listdir(path))
+		for i in range(num_files):
+		    matrix = pd.read_csv(path + "/" + str(i) + ".csv")
+		    weight = np.delete(matrix.as_matrix(), [0], axis=1)
+		    if ( (conv == True) and (i == 0) ):
+				weight = weight.reshape(32, 1, 5, 5)
+		    weights.append(weight)
+		return weights
+
+	def make_and_fit(self, path):
+		
+		input_X = T.tensor4('Input')
+		target_y = T.vector('Target', dtype='int32')
+
+		if (path == "Weight/Dense3"):
+			output_layer = architecture_one(input_X, weight = False)
+		elif (path == "Weight/Dense1"):
+			output_layer = architecture_two(input_X, weight = False)
+		elif (path == "Weight/Conv"):
+			output_layer = architecture_three(input_X, weight = False)
+		else:
+			print("I can't find architecure!")
+			return 0
+		y_predicted = lasagne.layers.get_output(output_layer)
+		all_weights = lasagne.layers.get_all_params(output_layer)
+
+		loss = lasagne.objectives.categorical_crossentropy(y_predicted,target_y).mean()
+		accuracy = lasagne.objectives.categorical_accuracy(y_predicted,target_y).mean()
+		updates_sgd = lasagne.updates.rmsprop(loss, all_weights,learning_rate=0.01)
+
+		train_fun = theano.function([input_X,target_y],[loss,accuracy],updates= updates_sgd)
+		self.accuracy_fun = theano.function([input_X,target_y],accuracy)
+		self.pred_fun = theano.function([input_X], y_predicted)
+
+		for epoch in range(self.num_epochs):
+		    self.new_epoch.emit(epoch + 1)
+		    train_err = 0
+		    train_acc = 0
+		    train_batches = 0
+		    start_time = time.time()
+		    for batch in iterate_minibatches(self.train_X, self.train_y, self.batch_size):
+		        inputs, targets = batch
+		        train_err_batch, train_acc_batch= train_fun(inputs, targets)
+		        train_err += train_err_batch
+		        train_acc += train_acc_batch
+		        train_batches += 1
+
+		    val_acc = 0
+		    val_batches = 0
+		    for batch in iterate_minibatches(self.val_X, self.val_y, self.batch_size):
+		        inputs, targets = batch
+		        val_acc += self.accuracy_fun(inputs, targets)
+		        val_batches += 1
+
+		    print("Epoch {} of {} took {:.3f}s".format(
+		        epoch + 1, self.num_epochs, time.time() - start_time))
+
+		    print("  training loss (in-iteration):\t\t{:.6f}".format(train_err / train_batches))
+		    print("  train accuracy:\t\t{:.2f} %".format(
+		        train_acc / train_batches * 100))
+		    print("  validation accuracy:\t\t{:.2f} %".format(
+		        val_acc / val_batches * 100))
+
+            	
+		if (path == "Weight/Dense3"):
+			self.put_weights(all_weights, path = "Weight/Dense3")
+		elif (path == "Weight/Dense1"):
+			self.put_weights(all_weights, path = "Weight/Dense1")
+		elif (path == "Weight/Conv"):
+			self.put_weights(all_weights, path = "Weight/Conv", conv = True)
+
+
+	def make_and_get(self, path):
+		input_X = T.tensor4('Input')
+		target_y = T.vector('Target', dtype='int32')
+		
+		if (path == "Weight/Dense3"):
+			output_layer = architecture_one(input_X, weight = True, weig = self.get_weight(path = "Weight/Dense3"))
+		elif (path == "Weight/Dense1"):
+			output_layer = architecture_two(input_X, weight = True, weig = self.get_weight(path = "Weight/Dense1"))
+		elif (path == "Weight/Conv"):
+			output_layer = architecture_three(input_X, weight = True, weig = self.get_weight(path = "Weight/Conv", conv = True))
+		else:
+			print("I can't find architecure!")
+			return 0
+		
+		y_predicted = lasagne.layers.get_output(output_layer)
+		all_weights = lasagne.layers.get_all_params(output_layer)
+
+		loss = lasagne.objectives.categorical_crossentropy(y_predicted,target_y).mean()
+		accuracy = lasagne.objectives.categorical_accuracy(y_predicted,target_y).mean()
+		updates_sgd = lasagne.updates.rmsprop(loss, all_weights,learning_rate=0.01)
+
+		train_fun = theano.function([input_X,target_y],[loss,accuracy],updates= updates_sgd)
+		self.accuracy_fun = theano.function([input_X,target_y],accuracy)
+		self.pred_fun = theano.function([input_X], y_predicted)
+		self.new_epoch.emit(self.num_epochs)
+
+
+	def make_and_check(self, path = "Weight"):
+		if (len(os.listdir(path)) == 0): self.make_and_fit(path = path)
+		else: self.make_and_get(path = path)
+
+	def get_accuracy(self):
+		if (self.acc == 0): 
+			test_acc = 0
+			test_batches = 0
+			for batch in iterate_minibatches(self.test_X, self.test_y, 500):
+			    inputs, targets = batch
+			    acc = self.accuracy_fun(inputs, targets)
+			    test_acc += acc
+			    test_batches += 1
+			print("Final results:")
+			print("  test accuracy:\t\t{:.2f} %".format(test_acc / test_batches * 100))
+			self.acc = test_acc / test_batches * 100
+			return test_acc / test_batches * 100
+		else:
+			return self.acc
+
+
+	def get_result(self, X):
+		self.get_accuracy()
+		y_pred = self.pred_fun(X)
+		
+		y_pred = np.array(y_pred)
+		#print y_pred
+		pred_num = y_pred.argmax()
+		pred_prob = y_pred[0, pred_num] * self.acc
+		print("Predict is {} with probabylity {:.2f} %".format(pred_num, pred_prob))
+		return pred_num, pred_prob
---
+++ Ver1.0/rt.py
@@ -0,0 +1,381 @@
+#!/usr/bin/python
+
+
+import sys
+from PyQt4 import QtCore, QtGui
+import NerNet
+import numpy as np
+
+class ScribbleArea(QtGui.QWidget):
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.setAttribute(QtCore.Qt.WA_StaticContents)
+        self.modified = False
+        self.scribbling = False
+        self.myPenWidth = 30
+        self.myPenColor = QtCore.Qt.black
+        self.image = QtGui.QImage()
+        self.lastPoint = QtCore.QPoint()
+
+    def openImage(self, fileName):
+        loadedImage = QtGui.QImage()
+        if not loadedImage.load(fileName):
+            return False
+
+        newSize = loadedImage.size().expandedTo(size())
+        self.resizeImage(loadedImage, newSize)
+        self.image = loadedImage
+        self.modified = False
+        self.update()
+        return True
+
+    def saveImage(self, fileName, fileFormat):
+        visibleImage = self.image
+        self.resizeImage(visibleImage, size())
+
+        if visibleImage.save(fileName, fileFormat):
+            self.modified = False
+            return True
+        else:
+            return False
+
+    def setPenColor(self, newColor):
+        self.myPenColor = newColor
+
+    def setPenWidth(self, newWidth):
+        self.myPenWidth = newWidth
+
+    def clearImage(self):
+        self.image.fill(QtGui.qRgb(255, 255, 255))
+        self.modified = True
+        self.update()
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.scribbling = True
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & QtCore.Qt.LeftButton) and self.scribbling:
+            self.drawLineTo(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and self.scribbling:
+            self.drawLineTo(event.pos())
+            self.scribbling = False
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        painter.drawImage(QtCore.QPoint(0, 0), self.image)
+        painter.end()
+
+    def resizeEvent(self, event):
+        if self.width() > self.image.width() or self.height() > self.image.height():
+            newWidth = max(self.width() + 128, self.image.width())
+            newHeight = max(self.height() + 128, self.image.height())
+            self.resizeImage(self.image, QtCore.QSize(400, 400))
+            self.update()
+
+        QtGui.QWidget.resizeEvent(self, event)
+
+    def drawLineTo(self, endPoint):
+        painter = QtGui.QPainter()
+        painter.begin(self.image)
+        painter.setPen(QtGui.QPen(self.myPenColor, self.myPenWidth,
+                                  QtCore.Qt.SolidLine, QtCore.Qt.RoundCap,
+                                  QtCore.Qt.RoundJoin))
+        painter.drawLine(self.lastPoint, endPoint)
+        painter.end()
+        self.modified = True
+
+        rad = self.myPenWidth / 2 + 2
+        self.update(QtCore.QRect(self.lastPoint, endPoint).normalized()
+                                         .adjusted(-rad, -rad, +rad, +rad))
+        self.lastPoint = QtCore.QPoint(endPoint)
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+        newImage = QtGui.QImage(newSize, QtGui.QImage.Format_RGB32)
+        newImage.fill(QtGui.qRgb(255, 255, 255))
+        painter = QtGui.QPainter()
+        painter.begin(newImage)
+        painter.drawImage(QtCore.QPoint(0, 0), image)
+        painter.end()
+        self.image = newImage
+
+    def prepareImage(self):
+        newSize = QtCore.QSize(28, 28)
+        retImage = self.image.scaled(newSize, transformMode = QtCore.Qt.SmoothTransformation) #transformMode = QtCore.Qt.SmoothTransformation
+        buf = np.ndarray((1, 1, 28, 28))
+        for i in xrange(28):
+            for j in xrange(28):
+                gray = QtGui.qGray(retImage.pixel(i, j))
+                retImage.setPixel(i, j, QtGui.QColor(gray, gray, gray).rgb())
+                buf[0][0][j][i] = 1-gray/255.
+        retImage.save("result.png")
+        return buf
+    
+    def isModified(self):
+        return self.modified
+
+    def penColor(self):
+        return self.myPenColor
+
+    def penWidth(self):
+        return self.myPenWidth
+
+class FormWidget(QtGui.QWidget):
+ 
+    def __init__(self, parent):        
+        super(FormWidget, self).__init__(parent)
+        self.scribbleArea = ScribbleArea()
+
+        self.layout = QtGui.QVBoxLayout(self)
+        self.buttonBox = QtGui.QHBoxLayout(self)
+
+        self.exitBtn = QtGui.QPushButton()
+        self.runBtn = QtGui.QPushButton()
+        self.aboutBtn = QtGui.QPushButton()
+        self.fitBtn = QtGui.QPushButton()
+        self.clearBtn = QtGui.QPushButton()
+
+        self.exitIcon = QtGui.QIcon('icons/exit.png')
+        self.fitIcon = QtGui.QIcon('icons/repeat.png')
+        self.runIcon = QtGui.QIcon('icons/play-button.png')
+        self.aboutIcon = QtGui.QIcon('icons/info.png')
+        self.clearIcon = QtGui.QIcon('icons/garbage.png')
+
+        self.exitBtn.setIcon(self.exitIcon)
+        self.fitBtn.setIcon(self.fitIcon)
+        self.runBtn.setIcon(self.runIcon)
+        self.aboutBtn.setIcon(self.aboutIcon)
+        self.clearBtn.setIcon(self.clearIcon)
+
+        self.ansLabel = QtGui.QLabel("Answer")
+        self.sureLabel = QtGui.QLabel("Sure")
+
+        self.buttonBox.addWidget(self.exitBtn)
+        self.buttonBox.addWidget(self.fitBtn)
+        self.buttonBox.addWidget(self.runBtn)
+        self.buttonBox.addWidget(self.aboutBtn)
+        self.buttonBox.addWidget(self.clearBtn)
+
+        self.layout.addLayout(self.buttonBox)
+        self.layout.addWidget(self.scribbleArea)
+        self.layout.addWidget(self.ansLabel)
+        self.layout.addWidget(self.sureLabel)
+        self.layout.setStretch(1, 10)
+        self.layout.setStretch(2, 1)
+        self.layout.setStretch(3, 1)
+
+
+class AboutWidget(QtGui.QWidget):
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setWindowTitle("About")
+
+        self.layout = QtGui.QHBoxLayout()
+        self.prepareLayout()
+        
+        self.setLayout(self.layout)
+        self.show()
+
+    def createImageLabels(self):
+        self.miptPixmap = QtGui.QPixmap("icons/eng.jpg").scaledToWidth(100)
+        self.miptImage = QtGui.QLabel()
+        self.miptImage.setPixmap(self.miptPixmap)
+
+        self.flaticonPixmap = QtGui.QPixmap("icons/flaticon-logo-footer.svg").scaledToWidth(100)
+        self.flaticonImage = QtGui.QLabel()
+        self.flaticonImage.setPixmap(self.flaticonPixmap)
+
+        self.qtPixmap = QtGui.QPixmap("icons/PyQt.png").scaledToWidth(100)
+        self.qtImage = QtGui.QLabel()
+        self.qtImage.setPixmap(self.qtPixmap)
+
+    def createLabels(self):
+        self.miptLabel = QtGui.QLabel("""Project of digit-recognition is made by students of Department of Radioengineering and Cybernetics 
+of Moscow Institute of Physics and Technology - Pavel Zakharov and Nikita Mokrov.\n""")
+        
+        self.flaticonLabel = QtGui.QLabel("""<div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a>
+         from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 
+         title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>\n""")
+        self.flaticonLabel.setOpenExternalLinks(True)
+        
+        self.theanoLabel = QtGui.QLabel("""Neural nets is based on <a href="http://deeplearning.net/software/theano/index.html">Theano</a> and <a href="https://lasagne.readthedocs.io/en/latest/">Lasagne</a> libraries:\n""")
+        self.theanoLabel.setOpenExternalLinks(True)
+        
+        self.qtLabel = QtGui.QLabel("""This project uses PyQt4. More about Qt library in corresponding dialog\n""")
+
+    def prepareLayout(self):
+        self.createLabels()
+        self.createImageLabels()
+
+        self.leftLayout = QtGui.QVBoxLayout()
+        self.rightLayout = QtGui.QVBoxLayout()
+    
+        self.leftLayout.addWidget(self.miptLabel)
+        self.rightLayout.addWidget(self.miptImage)
+        
+        self.leftLayout.addWidget(self.theanoLabel)
+        
+        self.leftLayout.addWidget(self.flaticonLabel)
+        self.rightLayout.addWidget(self.flaticonImage)
+        
+        self.leftLayout.addWidget(self.qtLabel)
+        self.rightLayout.addWidget(self.qtImage)
+
+        self.layout.addLayout(self.leftLayout)
+        self.layout.addLayout(self.rightLayout)
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent = None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.saveAsActs = []
+        self.centWidget = FormWidget(self)
+        self.setCentralWidget(self.centWidget)
+        self.test = None
+
+        self.createActions()
+        self.createMenus()
+
+        self.setWindowTitle(self.tr("Digit recognition v1"))
+        self.resize(500, 500)
+
+        self.connect(self.centWidget.exitBtn, QtCore.SIGNAL('clicked()'), self, QtCore.SLOT('close()'))
+        self.centWidget.fitBtn.clicked.connect(self.initializeNetwork)
+        self.centWidget.runBtn.clicked.connect(self.run)
+        self.centWidget.aboutBtn.clicked.connect(self.about)
+        self.centWidget.clearBtn.clicked.connect(self.centWidget.scribbleArea.clearImage)
+
+        self.networkReady = 0
+        self.progBar = QtGui.QProgressBar()
+        self.statusBar().addWidget(self.progBar)
+        self.labl = QtGui.QLabel()
+        self.statusBar().addPermanentWidget(self.labl)
+        self.setlabl()
+
+        self.timer = QtCore.QBasicTimer()
+        #self.initializeNetwork()
+        self.net = NerNet.NerNet()
+        self.net.init_data()
+        self.net.signalConnect(self.epochEvent)
+        #Need to conect
+
+    def setlabl(self):
+        if self.networkReady == 100:
+            self.labl.setText("Ready")
+        else:
+            self.labl.setText("Initializing..")
+
+    def run(self):
+        print "Run"
+        self.net.get_result(self.centWidget.scribbleArea.prepareImage())
+        #Get result with net
+        #self.net.result(...)
+
+
+    def initializeNetwork(self):
+        #Fit a net
+        #self.net.get_accuracy() - init area
+        self.networkReady = 0
+        self.centWidget.runBtn.setEnabled(False)
+        self.centWidget.fitBtn.setEnabled(False)
+        self.net.make_and_check(path = "Weight/Conv")
+    
+    def epochEvent(self, epoch):
+        if epoch == self.net.num_epochs:#10
+            self.centWidget.runBtn.setEnabled(True)
+            self.centWidget.fitBtn.setEnabled(True)
+            self.progBar.setValue(100)
+            self.setlabl()
+            self.networkReady = 0
+            return
+        self.networkReady = self.networkReady + 100 / self.net.num_epochs #10
+        self.progBar.setValue(self.networkReady)
+        self.setlabl()
+
+    def open(self):
+        if self.maybeSave():
+            fileName = QtGui.QFileDialog.getOpenFileName(self,
+                                                         self.tr("Open File"),
+                                                         QtCore.QDir.currentPath())
+            if not fileName.isEmpty():
+                self.centWidget.scribbleArea.openImage(fileName)
+
+    def penColor(self):
+        newColor = QtGui.QColorDialog.getColor(self.centWidget.scribbleArea.penColor())
+        if newColor.isValid():
+            self.centWidget.scribbleArea.setPenColor(newColor)
+
+    def penWidth(self):
+        newWidth, ok = QtGui.QInputDialog.getInteger(self, self.tr("Scribble"),
+                                               self.tr("Select pen width:"),
+                                               self.centWidget.scribbleArea.penWidth(),
+                                               1, 50, 1)
+        if ok:
+            self.centWidget.scribbleArea.setPenWidth(newWidth)
+
+    def about(self):
+        self.aboutWindow = AboutWidget()
+
+    def createActions(self):
+        self.openAct = QtGui.QAction(self.tr("&Open..."), self)
+        self.openAct.setShortcut(self.tr("Ctrl+O"))
+        self.connect(self.openAct, QtCore.SIGNAL("triggered()"), self.open)
+        
+        self.exitAct = QtGui.QAction(self.tr("E&xit"), self)
+        self.exitAct.setShortcut(self.tr("Ctrl+Q"))
+        self.connect(self.exitAct, QtCore.SIGNAL("triggered()"), self, QtCore.SLOT("close()"))
+
+        self.penColorAct = QtGui.QAction(self.tr("&Pen Color..."), self)
+        self.connect(self.penColorAct, QtCore.SIGNAL("triggered()"),
+                     self.penColor)
+
+        self.penWidthAct = QtGui.QAction(self.tr("Pen &Width..."), self)
+        self.connect(self.penWidthAct, QtCore.SIGNAL("triggered()"),
+                     self.penWidth)
+
+        self.clearScreenAct = QtGui.QAction(self.tr("&Clear Screen"), self)
+        self.clearScreenAct.setShortcut(self.tr("Ctrl+L"))
+        self.connect(self.clearScreenAct, QtCore.SIGNAL("triggered()"),
+                     self.centWidget.scribbleArea.clearImage)
+
+        self.aboutAct = QtGui.QAction(self.tr("&About"), self)
+        self.connect(self.aboutAct, QtCore.SIGNAL("triggered()"), self.about)
+
+        self.aboutQtAct = QtGui.QAction(self.tr("About &Qt"), self)
+        self.connect(self.aboutQtAct, QtCore.SIGNAL("triggered()"),
+                     QtGui.qApp, QtCore.SLOT("aboutQt()"))
+
+    def createMenus(self):
+        self.fileMenu = QtGui.QMenu(self.tr("&File"), self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.optionMenu = QtGui.QMenu(self.tr("&Options"), self)
+        self.optionMenu.addAction(self.penColorAct)
+        self.optionMenu.addAction(self.penWidthAct)
+        self.optionMenu.addSeparator()
+        self.optionMenu.addAction(self.clearScreenAct)
+
+        self.helpMenu = QtGui.QMenu(self.tr("&Help"), self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.optionMenu)
+        self.menuBar().addMenu(self.helpMenu)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
